8b210211737f207b396ddf9a703d3c7c4ec9230c
Generaalguan <33372@ma-web.nl>
2025-03-04 11:56:58 +0100

    heb crud toegevoegd aan skills

---
+++ Backend/app/Http/Controllers/SkillsController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Skills;
+use Illuminate\Http\Request;    
+
+class skillsController extends Controller
+{
+    public function index($id = null)
+    {
+
+        $skills = Skills::all();
+
+        return response()->json($skills);
+    }
+    public function create(Request $request)
+    {
+        $skills = new Skills;
+        $skills->title = $request->title;
+        $skills->description = $request->description;
+        $skills->example = $request->example;
+        $skills->icon = $request->icon;
+        $skills->save();
+        return response()->json(['message' => 'Skill successfull aan gemaakt', 'status' => 'success'], 200);
+    }
+
+
+    public function edit(Request $request, $id)
+    {
+        $skills = Skills::find($id);
+
+        if (!$skills) {
+            return response()->json(['message' => 'Skill niet gevonden', 'status' => 'error'], 404);
+        }
+        $skills = new Skills;
+        $skills->title = $request->title;
+        $skills->description = $request->description;
+        $skills->example = $request->example;
+        $skills->icon = $request->icon;
+    
+        $skills->save();
+    
+        return response()->json(['message' => 'Skill successfull geupdate', 'status' => 'success'], 200);
+    }
+
+    public function delete($id)
+    {
+        $skills = Skills::find($id);
+
+        if (!$skills) {
+            return response()->json(['message' => 'Skill niet gevonden', 'status'=> 'error'], 404);
+        }
+
+        $skills->delete();
+
+        return response()->json(['message' => 'Skill successfull verwijderd', 'status'=> 'succes'], 200);
+    }
+}
