66e9e476ef904f4f04ff5dfbdb2bab90708a93c9
Generaalguan <33372@ma-web.nl>
2025-03-04 09:56:08 +0100

    zo deleten

--- Backend/app/Models/projects.php
+++ Backend/app/Models/projects.php
@@ -2,13 +2,7 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Fotos;
-class projects extends Model
+class Projects extends Model
 {
-    protected $table = 'projects';
-
-    public function fotos()
-    {
-        return $this->hasMany(Fotos::class, 'projectId', 'id');
-    }
+    protected $table = 'projecten';
 }
